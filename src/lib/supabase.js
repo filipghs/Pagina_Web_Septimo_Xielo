dd5c6d4474d921096dd7a6a13825d9e0929cd89d
Filip_8002 <fjosegarzonherrera@gmail.com>
2026-06-09 23:11:28 -0500

    Version 1.2 Mejora de seguridad en las variables

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://weurxasrufgukkxlvxxo.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndldXJ4YXNydWZndWtreGx2eHhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3NzAxMDAsImV4cCI6MjA5NjM0NjEwMH0.9a7PMrwtqV_OWqaEEOpnlx70OGDtD0bHleqBus33GdM";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
